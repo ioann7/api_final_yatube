34ad9cfc614d71593506451202e7af1a31111e5a
chakchakk <ioann.chimrov@gmail.com>
2022-11-30 12:41:58 +0300

    create new viewset

---
+++ yatube_api/api/viewsets.py
@@ -0,0 +1,14 @@
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
+
+
+class CreateListViewSet(mixins.CreateModelMixin,
+                        mixins.ListModelMixin,
+                        GenericViewSet):
+    """
+    A viewset that provides `create` and `list` actions.
+
+    To use it, override the class and set the `.queryset` and
+    `.serializer_class` attributes.
+    """
+    pass
